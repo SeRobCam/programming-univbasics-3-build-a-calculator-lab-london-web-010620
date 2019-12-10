67416be3efbb9ab57ae3a38f1777e046cd6f3d23
Sergio Robles <sroblesc@gmail.com>
2019-12-10 12:21:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  subs = num1 - num2
+  return subs
 end
 
 def division(num1, num2)
-
+  divis = num1 / num2
+  return divis
 end
 
 def multiplication(num1, num2)
-
+  multip = num1 * num2
+  return multip
 end
 
 def modulo(num1, num2)
